afe44b9c636c1296c950f6a4655731bd3e7827bc
Navid Salehi <52621302+navidslh2@users.noreply.github.com>
2025-06-12 12:15:50 +0330

    feat(navbar): complete component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      navbar
+    <div className="navbar">
+      <div className="logo">
+        <img src="../../../public/logo.png" alt="logo" />
+        <p className="name">نوید صالحی</p>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="search" className="icon" />
+        <img src="/app.svg" alt="app" className="icon" />
+        <img src="/expand.svg" alt="expand" className="icon" />
+        <div className="notification">
+          <img src="/notification.svg" alt="" />
+          <span>4</span>
+        </div>
+        <div className="user">
+          <span>مجید</span>
+        </div>
+        <img src="/setting.svg" alt="" className="icon" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
